bfd83d226f3d9650af7d169ebb7016e06238ce89
Jose Borunda <jose.h.borunda@gmail.com>
2019-08-05 20:36:11 -0600

    added google maps api, along with js, css, and html coding associated with add

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,8 +20,91 @@
 //   }).then(function(response){
 //    $("#json-view").text(JSON.stringify(response))
 //   });
-    
-    
+
+//  googlemaps   
+function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+      var x = document.getElementById("location");
+      x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+function showPosition(position) {
+  var x = document.getElementById("location");
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude; 
+  var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=O4ZKxAFY8EOCdyuM8hx8sWLZSF8gkBHF&latlong="+latlon,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                var e = document.getElementById("events");
+                e.innerHTML = json.page.totalElements + " events found.";
+                showEvents(json);
+                initMap(position, json);
+             },
+    error: function(xhr, status, err) {
+                console.log(err);
+             }
+  });
+
+}
+
+function showError(error) {
+  switch(error.code) {
+      case error.PERMISSION_DENIED:
+          x.innerHTML = "User denied the request for Geolocation."
+          break;
+      case error.POSITION_UNAVAILABLE:
+          x.innerHTML = "Location information is unavailable."
+          break;
+      case error.TIMEOUT:
+          x.innerHTML = "The request to get user location timed out."
+          break;
+      case error.UNKNOWN_ERROR:
+          x.innerHTML = "An unknown error occurred."
+          break;
+  }
+}
+
+
+function showEvents(json) {
+for(var i=0; i<json.page.size; i++) {
+  $("#events").append("<p>"+json._embedded.events[i].name+"</p>");
+}
+}
+
+
+function initMap(position, json) {
+var mapDiv = document.getElementById('map');
+var map = new google.maps.Map(mapDiv, {
+  center: {lat: position.coords.latitude, lng: position.coords.longitude},
+  zoom: 10
+});
+for(var i=0; i<json.page.size; i++) {
+  addMarker(map, json._embedded.events[i]);
+}
+}
+
+function addMarker(map, event) {
+var marker = new google.maps.Marker({
+  position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
+  map: map
+});
+marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+console.log(marker);
+}
+
+
+
+
+getLocation();
 // });
 //functions need to be created for when a city is entered, events from multiple API's need to populate to its designated page. need to come up with a limit of how many events will be displayed. on click to be at the end.
 var cities = ["Denver", "Austin", "seattle", "Dallas"];
